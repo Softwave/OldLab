fd0b0d25300e5558c0cd684fd444e791abd2941f
J. Leyba <jcleyba92@gmail.com>
2016-01-14 16:45:56 -0700

    Updated starter and added Diaspora

--- starter/Main.js
+++ starter/Main.js
@@ -13,94 +13,106 @@ var stats;
 var controls;
 var clock;
 
+var controlsEnabled = true;
+var statsEnabled = true;
+
+var mesh;
+
 //GUI
 var guiParams = {
-	rotateX: 0.005,
-	rotateY: 0.005, 
-	rotateZ: 0.005
+    rotateX: 0.005,
+    rotateY: 0.005, 
+    rotateZ: 0.005
 };
+var gui = new dat.GUI();
+gui.add(guiParams, 'rotateX', 0.0, 0.1).name('rotX');
+gui.add(guiParams, 'rotateY', 0.0, 0.1).name('rotY');
+gui.add(guiParams, 'rotateZ', 0.0, 0.1).name('rotZ');
+gui.close();
 
 document.addEventListener("DOMContentLoaded", startApp, false);
 
 function startApp() {
-	init();
-	animate();
+    init();
+    animate();
 }
 
 function init() {
-	//GUI
-	var gui = new dat.GUI();
-	gui.add(guiParams, 'rotateX', 0.0, 0.1).name('rotX');
-	gui.add(guiParams, 'rotateY', 0.0, 0.1).name('rotY');
-	gui.add(guiParams, 'rotateZ', 0.0, 0.1).name('rotZ');
-	gui.close();
+    //Setup Container
+    container = document.getElementById('container');
+    renderer = new THREE.WebGLRenderer({antialias: true});
+    renderer.setClearColor(0x000000, 1.0);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    container.appendChild(renderer.domElement);
 
-	//Setup Container
-	container = document.getElementById('container');
-	renderer = new THREE.WebGLRenderer({antialias: true});
-	renderer.setClearColor(0x000000, 1.0);
-	renderer.setSize(window.innerWidth, window.innerHeight);
-	container.appendChild(renderer.domElement);
+    //Scene
+    scene = new THREE.Scene();
 
-	//Scene
-	scene = new THREE.Scene();
+    //Camera
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000);
+    camera.position.z = 80;
 
-	//Camera
-	camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000);
-	camera.position.z = 80;
+    //Stats
+    stats = new Stats();
+    stats.domElement.style.position = 'absolute';
+    stats.domElement.style.top = '0px';
+    document.body.appendChild(stats.domElement);
+    document.body.appendChild(renderer.domElement);
 
-	//Stats
-	stats = new Stats();
-	stats.domElement.style.position = 'absolute';
-	stats.domElement.style.top = '0px';
-	document.body.appendChild(stats.domElement);
-	document.body.appendChild(renderer.domElement);
+    //Controls
+    controls = new THREE.TrackballControls(camera, renderer.domElement);
+    controls.rotateSpeed = 0.4;
+    controls.noZoom = false; 
+    controls.noPan = false;
+    controls.staticMoving = false;
+    controls.minDistance = 10;
+    controls.maxDistance = 4000;
+    controls.dynamicDampingFactor = 0.2; 
 
-	//Controls
-	controls = new THREE.TrackballControls(camera, renderer.domElement);
-	controls.rotateSpeed = 0.4;
-	controls.noZoom = false; 
-	controls.noPan = false;
-	controls.staticMoving = false;
-	controls.minDistance = 10;
-	controls.maxDistance = 4000;
-	controls.dynamicDampingFactor = 0.2; 
+    //Lights
+    var ambientLight = new THREE.AmbientLight(0x111111);
+    scene.add(ambientLight);
 
-	//Lights
-	var ambientLight = new THREE.AmbientLight(0x111111);
-	scene.add(ambientLight);
+    //Geometry and material
+    var geometry = new THREE.BoxGeometry(10, 10, 10);
+    var material = new THREE.MeshBasicMaterial({
+        color: 0xffffff,
+        shading: THREE.FlatShading,
+        wireframe: true
+    });
 
-	var geometry = new THREE.BoxGeometry(10, 10, 10);
-	var material = new THREE.MeshBasicMaterial({
-		color: 0xffffff,
-		shading: THREE.FlatShading,
-		wireframe: true
-	});
+    //Add the mesh 
+    mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh);
 
-	mesh = new THREE.Mesh(geometry, material);
-	scene.add(mesh);
-
-	window.addEventListener('resize', onWindowResize, false);
+    //Window resize
+    window.addEventListener('resize', onWindowResize, false);
 }
 
 function animate() {
-	requestAnimationFrame(animate);
+    requestAnimationFrame(animate);
 
-	mesh.rotation.x += guiParams.rotateX;
-	mesh.rotation.y += guiParams.rotateY;
-	mesh.rotation.z += guiParams.rotateZ;
+    mesh.rotation.x += guiParams.rotateX;
+    mesh.rotation.y += guiParams.rotateY;
+    mesh.rotation.z += guiParams.rotateZ;
 
-	controls.update();
+    if (controlsEnabled) {
+        controls.update();
+    }
 
-	render();
+    if (statsEnabled) {
+        stats.update();
+    }
+
+    render();
 }
 
 function render() {
-	renderer.render(scene, camera);
+    renderer.render(scene, camera);
 }
 
 function onWindowResize() {
-	camera.aspect = window.innerWidth / window.innerHeight;
+    camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
 }
